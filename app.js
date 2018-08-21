156a4578028bf634ad22673ff8aacbcc382b3e5b
Sam Mills (Henchman) <sam.mills@hench.space>
2018-08-21 16:44:15 +0100

    Cleaned UI, added world status print

--- app.js
+++ app.js
@@ -37,21 +37,23 @@ function loop(timestamp) {
     window.requestAnimationFrame(loop);
 }
 
-let x = 0; let y = 0;
+let x = 0, y = 0;
 
 function update(elapsed) {
     fps = 1000 / elapsed;
 
     // Generate world 1 chunk per loop
     if (x < MAP_SIZE * 2) {
+        world.status = "Generating: (" + x + ", " + y + ")";
         world.generateChunk(x, y);
         x++;
-        console.log(x, y);
     } else if (y < MAP_SIZE - 1) {
+        world.status = "Generating: (" + x + ", " + y + ")";
         x = 0;
         y++;
         world.generateChunk(x, y);
-        console.log(x, y);
+    } else {
+        world.status = "Idle";
     }
 }
 
@@ -62,7 +64,7 @@ function draw(evt) {
     context.clearRect(0, 0, canvas.width, canvas.height);
     world.renderWorldView(mapPosition, canvas.width, canvas.height);
 
-    context.clearRect(10, 10, 175, 250);
+    context.clearRect(10, 10, 175, 300);
     drawGrid(CHUNK_SIZE, CHUNK_SIZE / 2);
 
     drawText("FPS: " + Math.round(fps), {x: 20, y: 30}, "#FFFFFF");
@@ -75,20 +77,23 @@ function draw(evt) {
     try {
         drawText("Chunk X: " + chunkReference.Cx, {x: 20, y: 70}, "#FFFFFF");
         drawText("Chunk Y: " + chunkReference.Cy, {x: 20, y: 80}, "#FFFFFF");
-        drawText("Block X: " + chunkReference.x, {x: 20, y: 90}, "#FFFFFF");
-        drawText("Block Y: " + chunkReference.y, {x: 20, y: 100}, "#FFFFFF");
+        drawText("Tile X: " + chunkReference.x, {x: 20, y: 90}, "#FFFFFF");
+        drawText("Tile Y: " + chunkReference.y, {x: 20, y: 100}, "#FFFFFF");
 
         const elevation = world.getElevation(chunkReference);
-        const moisture = world.getMoisture(chunkReference);
-        const latitude = world.getLatitude(chunkReference);
-        const temperature = world.getTemperature(chunkReference);
-        const biome = world.getBiome(elevation, moisture, temperature);
-
         drawText("Elevation: " + elevation, {x: 20, y: 150}, "#FFFFFF");
+
+        const moisture = world.getMoisture(chunkReference);
         drawText("Moisture: " + moisture, {x: 20, y: 160}, "#FFFFFF");
-        drawText("Biome: " + biome + " (" + tiles[biome].name + ")", {x: 20, y: 170}, "#FFFFFF");
-        drawText("Latitude: " + latitude, {x: 20, y: 180}, "#FFFFFF");
-        drawText("Temperature: " + temperature, {x: 20, y: 190}, "#FFFFFF");
+
+        const latitude = world.getLatitude(chunkReference);
+        drawText("Latitude: " + latitude, {x: 20, y: 170}, "#FFFFFF");
+
+        const temperature = world.getTemperature(chunkReference);
+        drawText("Temperature: " + temperature, {x: 20, y: 180}, "#FFFFFF");
+
+        const biome = world.getBiomeValue(chunkReference);
+        drawText("Biome: " + biome + " (" + tiles[biome].name + ")", {x: 20, y: 190}, "#FFFFFF");
 
     } catch (e) {
         console.error(e);
@@ -96,8 +101,11 @@ function draw(evt) {
 
     drawText("Map Offset X: " + mapPosition.x, {x: 20, y: 120}, "#FFFFFF");
     drawText("Map Offset Y: " + mapPosition.y, {x: 20, y: 130}, "#FFFFFF");
-    drawText("Chunks Rendered: " + (world.renderedChunks + "/" + world.totalChunks), {x: 20, y: 210}, "#FFFFFF");
-    drawText("Chunks Drawn: " + world.chunksDrawn, {x: 20, y: 220}, "#FFFFFF");
+
+    drawText("Chunks Rendered: " + (world.renderedChunks + "/" + world.totalChunks), {x: 20, y: 230}, "#FFFFFF");
+    drawText("Chunks Drawn: " + world.chunksDrawn, {x: 20, y: 240}, "#FFFFFF");
+
+    drawText("Status: " + world.status, {x: 20, y: 270}, "#FFFFFF");
 }
 
 function keypress(e) {
@@ -108,6 +116,7 @@ function keypress(e) {
         case 38: mapPosition.y -= 50; break; //Up key
         case 39: mapPosition.x += 50; break; //Right key
         case 40: mapPosition.y += 50; break; //Down key
+        case 82: mapPosition.y = 0; mapPosition.x = 0; break; //Reset Map Position
         //default: alert(code); //Everything else
     }
 }
@@ -128,12 +137,13 @@ function getMousePos(canvas, evt) {
 }
 
 function mouseDrag (){
-        if(mousePressed) {
-            mapPosition.x -= (mousePosition.x - lastMouseClickPosition.x)
-            mapPosition.y -= (mousePosition.y - lastMouseClickPosition.y)
-        }
-        lastMouseClickPosition = mousePosition;
+    if(mousePressed) {
+        mapPosition.x -= (mousePosition.x - lastMouseClickPosition.x);
+        mapPosition.y -= (mousePosition.y - lastMouseClickPosition.y);
+    }
+    lastMouseClickPosition = mousePosition;
 }
+
 canvas.addEventListener('mousemove', function(evt) {
     mousePosition = getMousePos(canvas, evt);
     mouseDrag();
